58c6b3104701f3cbbea1f8c7bed39f1d16943984
Daria <alyoshinadasha4@gmail.com>
2022-04-19 17:52:15 +0300

    App screenshots added

--- js/main.js
+++ js/main.js
@@ -41,5 +41,25 @@ $(document).ready(function(){
         }
     }
 });
+ 
+// carousel screenshots
+$('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+            
+        }
+    }
+});
 
 });
